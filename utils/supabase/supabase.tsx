6bee2d8af135b565de26d8bffe48fd83b6a866ab
Rishabh Kumar <rishabh@Rishabhs-MacBook-Pro.local>
2024-01-24 18:04:12 -0500

    testing w/out env

--- utils/supabase/supabase.tsx
+++ utils/supabase/supabase.tsx
@@ -1,7 +1,10 @@
 // supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const NEXT_PUBLIC_SUPABASE_URL1= "https://tvjhpizmtgylohkzsotr.supabase.co"
+const NEXT_PUBLIC_SUPABASE_ANON_KEY1="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2amhwaXptdGd5bG9oa3pzb3RyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU5NDcxMDYsImV4cCI6MjAyMTUyMzEwNn0.LRZ7m-ffoBrnTlfn2kvutj2CM2WUNpp5znEjAHZvnt4"
+
+const supabaseUrl = NEXT_PUBLIC_SUPABASE_URL1;
+const supabaseAnonKey = NEXT_PUBLIC_SUPABASE_ANON_KEY1;
 
 export const supabase = createClient(supabaseUrl!, supabaseAnonKey!);
